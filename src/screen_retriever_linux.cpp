acd8194b27392c6ce02c55b25816ec26fdcff3b6
LiJianying <lijy91@foxmail.com>
2025-03-31 23:15:46 +0800

    feat: Implement screen retriever part methods for Linux

--- src/screen_retriever_linux.cpp
+++ src/screen_retriever_linux.cpp
@@ -1,9 +1,37 @@
+#include <gtk/gtk.h>
 #include <iostream>
 #include "screen_retriever.h"
 
 namespace nativeapi {
 
+static Display CreateDisplayFromGdkMonitor(GdkMonitor* monitor,
+                                           bool isFirstScreen) {
+  Display display;
+
+  display.id = "";
+  display.name = gdk_monitor_get_model(monitor);
+
+  GdkRectangle frame;
+  gdk_monitor_get_geometry(monitor, &frame);
+
+  display.width = frame.width;
+  display.height = frame.height;
+
+  GdkRectangle workarea_rect;
+  gdk_monitor_get_workarea(monitor, &workarea_rect);
+
+  display.visibleSizeWidth = workarea_rect.width;
+  display.visibleSizeHeight = workarea_rect.height;
+  display.visiblePositionX = workarea_rect.x;
+  display.visiblePositionY = workarea_rect.y;
+
+  display.scaleFactor = gdk_monitor_get_scale_factor(monitor);
+
+  return display;
+}
+
 ScreenRetriever::ScreenRetriever() {
+  gtk_init(nullptr, nullptr);
   // Constructor implementation
   std::cout << "ScreenRetriever initialized" << std::endl;
 }
@@ -14,26 +42,33 @@ ScreenRetriever::~ScreenRetriever() {
 }
 
 Point ScreenRetriever::GetCursorScreenPoint() {
+  GdkDisplay* display = gdk_display_get_default();
+  GdkSeat* seat = gdk_display_get_default_seat(display);
+  GdkDevice* pointer = gdk_seat_get_pointer(seat);
+
+  int x, y;
+  gdk_device_get_position(pointer, NULL, &x, &y);
+
   // Empty implementation
   Point point;
-  point.x = 0.0;
-  point.y = 0.0;
+  point.x = x;
+  point.y = y;
   return point;
 }
 
 Display ScreenRetriever::GetPrimaryDisplay() {
-  // Empty implementation
-  Display display;
-  display.id = "display-1";
-  display.name = "Linux Display";
-  display.width = 1920.0;
-  display.height = 1080.0;
-  display.visiblePositionX = 0.0;
-  display.visiblePositionY = 0.0;
-  display.visibleSizeWidth = 1920.0;
-  display.visibleSizeHeight = 1080.0;
-  display.scaleFactor = 1.0;
-  return display;
+  GdkDisplay* display = gdk_display_get_default();
+  GdkMonitor* monitor = gdk_display_get_primary_monitor(display);
+
+  // opt: fallback if there's no primary monitor
+  if (monitor == nullptr) {
+    int monitor_count = gdk_display_get_n_monitors(display);
+    if (monitor_count > 0) {
+      monitor = gdk_display_get_monitor(display, 0);
+    }
+  }
+
+  return CreateDisplayFromGdkMonitor(monitor, true);
 }
 
 std::vector<Display> ScreenRetriever::GetAllDisplays() {
